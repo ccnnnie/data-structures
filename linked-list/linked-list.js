bde6c767f9315fdfa6b20c42e0c463d0fc27ffab
Connie Lok <conniehlok@gmail.com>
2020-08-02 00:14:52 -0400

    LL: pop, shift, unshift, get, set, insert

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -24,11 +24,91 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
-}
 
-// const list = new SinglyLinkedList();
-// list.push('HELLO');
-// list.push('WORLD');
-// console.log(list);
+  pop() {
+    let currNode = this.head;
+    if (!currNode) return undefined;
+    else if (currNode === this.tail) {
+      this.tail = null;
+      this.head = null;
+    } else {
+      while (currNode) {
+        if (currNode.next === this.tail) {
+          this.tail = currNode;
+          currNode.next = null;
+        }
+        currNode = currNode.next;
+      }
+    }
+    this.length--;
+    return this;
+  }
+
+  shift() {
+    if (!this.head) return undefined;
+    else if (this.head === this.tail) {
+      this.tail = null;
+      this.head = null;
+    } else {
+      this.head = this.head.next;
+    }
+    this.length--;
+    return this;
+  }
+
+  unshift(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  get(idx) {
+    if (idx < 0 || idx >= this.length) return null;
+    let currIdx = 0;
+    let currNode = this.head;
+    while (currIdx !== idx) {
+      currNode = currNode.next;
+      currIdx++;
+    }
+    return currNode;
+  }
+
+  set(val, idx) {
+    const node = this.get(idx);
+    if (!node) return false;
+    else {
+      node.val = val;
+      return true;
+    }
+  }
+
+  insert(val, idx) {
+    if (idx < 0 || idx > this.length) return false;
+    if (idx === 0) this.unshift(val);
+    else if (idx === this.length) this.push(val);
+    else {
+      let currIdx = 0;
+      let currNode = this.head;
+      const newNode = new Node(val);
+      while (currNode) {
+        if (currIdx === idx - 1) {
+          newNode.next = currNode.next;
+          currNode.next = newNode;
+        }
+        currNode = currNode.next;
+        currIdx++;
+      }
+    }
+    this.length++;
+    return this;
+  }
+}
 
 module.exports = SinglyLinkedList;
--- linked-list/linked-list.spec.js
+++ linked-list/linked-list.spec.js
@@ -9,7 +9,7 @@ describe('A singly linked list', () => {
   test('can add first node to list', () => {
     list.push('HELLO');
     expect(list.head).toEqual(list.tail);
-    expect(list.tail).toMatchObject({ val: 'HELLO' });
+    expect(list.tail.val).toBe('HELLO');
   });
 
   test('can add two nodes to list', () => {
@@ -17,6 +17,105 @@ describe('A singly linked list', () => {
     expect(list.length).toEqual(2);
     expect(list.head).toEqual({ val: 'HELLO', next: list.tail });
     expect(list.head.next).toEqual(list.tail);
-    expect(list.tail).toMatchObject({ val: 'WORLD' });
+    expect(list.tail.val).toBe('WORLD');
+  });
+
+  test('can add multiple items to the list', () => {
+    list
+      .push(1)
+      .push(2)
+      .push(3)
+      .push(4)
+      .push(5);
+    expect(list.tail.next).toBe(null);
+    expect(list.head.val).toBe(1);
+    expect(list.head.next.val).toBe(2);
+    expect(list.head.next.next.val).toBe(3);
+    expect(list.tail).toEqual(list.head.next.next.next.next);
+    expect(list.tail.val).toBe(5);
+  });
+
+  test('can remove from the end of the list', () => {
+    list
+      .push(1)
+      .push(2)
+      .pop();
+    expect(list.length).toBe(1);
+    expect(list.tail.val).toBe(1);
+    expect(list.head).toEqual(list.tail);
+    expect(list.tail.next).toBe(null);
+    list.pop();
+    expect(list.length).toBe(0);
+    expect(list.tail).toBe(null);
+    expect(list.head).toBe(null);
+  });
+
+  test('can remove from the beginning of the list', () => {
+    list
+      .push(1)
+      .push(2)
+      .push(3)
+      .shift();
+    expect(list.length).toBe(2);
+    expect(list.head.val).toBe(2);
+    list.shift();
+    expect(list.length).toBe(1);
+    expect(list.head.val).toBe(3);
+    expect(list.head).toEqual(list.tail);
+    list.shift();
+    expect(list.length).toBe(0);
+    expect(list.head).toBe(null);
+    expect(list.tail).toBe(null);
+  });
+
+  test('can add to the beginning of the list', () => {
+    list
+      .unshift(3)
+      .unshift(2)
+      .unshift(1);
+    expect(list.length).toBe(3);
+    expect(list.head.val).toBe(1);
+    expect(list.tail.val).toBe(3);
+    expect(list.tail).toEqual(list.head.next.next);
+  });
+
+  test('can get a node by its position in the list', () => {
+    list
+      .push('tacos')
+      .push('fries')
+      .push('chicken');
+    const tacos = list.get(0);
+    expect(tacos).toEqual(list.head);
+    expect(tacos.val).toBe('tacos');
+    const fries = list.get(1);
+    expect(fries).toEqual(list.head.next);
+    expect(fries.val).toBe('fries');
+    const chicken = list.get(2);
+    expect(chicken).toEqual(list.tail);
+    expect(chicken.val).toBe('chicken');
+  });
+
+  test('can change the value of a node by its position in the list', () => {
+    list
+      .push('tacos')
+      .push('fries')
+      .push('chicken')
+      .set('vegetables', 1);
+    const vegetables = list.get(1);
+    expect(vegetables).toEqual(list.head.next);
+    expect(vegetables.next).toEqual(list.tail);
+    expect(vegetables.val).toBe('vegetables');
+  });
+
+  test('can insert a node at a specified position', () => {
+    list
+      .push('Jan')
+      .push('Mar')
+      .push('Apr')
+      .insert('Feb', 1);
+    expect(list.head.next.val).toBe('Feb');
+    expect(list.head.next.next.val).toBe('Mar');
+    list.insert('May', 4);
+    expect(list.tail.val).toBe('May');
   });
 });
